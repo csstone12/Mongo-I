42113a5d8dbb70d623c1563b757a41fc9da5ae27
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-04-10 17:04:41 -0400

    Stone Chris | Mongo-I
    Stone Chris | Mongo-I

--- friends/friendController.js
+++ friends/friendController.js
@@ -14,7 +14,7 @@ router
       });
   })
   .post((req, res) => {
-    const { firstName, lastName, age} = req.body
+    const {firstName, lastName, age} = req.body
     if(!firstName || !lastName || !age){
         res.status(400).json({err: 'Please provide firstName, lastName and age for the friend'})
     }
@@ -22,14 +22,66 @@ router
         res.status(400).json({err: 'Age must be a number between 1 and 120'})
     }
 
-    const friend = new Friend ({ firstName, lastName, age});
+    const friend = new Friend ({firstName, lastName, age});
     friend
       .save()
       .then(savedFriend => {
         res.status(201).json(savedFriend);
       })
       .catch(err => res.status(500).json({err:'There was an error while saving the friend to the database.'}));
+  })
+router
+  .route('/:id')
+  .get((req, res) => {
+    Friend.findById(req.params.id)
+      .then(friends => {
+        res.status(200).json(friends);
+      })
+      .catch(err => {
+        res.status(500).json({err: 'The friends information could not be retrieved'});
+      });
+  })
+  .put((req, res) =>{
+    const {firstName, lastName, age} = req.body;
+    Friend
+      .findByIdAndUpdate(req.params.id, req.body)
+      .then(response => {
+        if (response===null){
+          res.status(404).json({ message: 'not found'});
+        }
+        else{
+          res.status(200).json(response);
+        }
+      })
+      .catch(err => {
+        if (err.name==='CastError'){
+          res.status(400).json({
+            message: 'The ID provided is invalid. Please try again.',
+          });
+        }
+        else {
+          res
+            .status(500)
+            .json({ errorMessage: 'The Friend could not be removed', err});
+        }
+      })
+  })
+  .delete((req, res) =>{
+    const {id} = req.params;
+    Friend.findByIdAndRemove(id)
+      .then(response => {
+        if (response===null){
+          res.status(404).json({message: 'not found'});
+        }
+        else{
+          res.status(200).json(response);
+        }
+      })
+      .catch(err => {
+        res.status(500).json({err: 'The friends information could not be retrieved'});
+      });
   });
+
 
 
 module.exports = router;
